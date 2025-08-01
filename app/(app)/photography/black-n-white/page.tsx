2a8a402e43767eca329dbe8def9b7929a274ea0e
enricomilli <enricomilli666@gmail.com>
2025-08-01 11:07:44 +0200

    fix: build errors for 1st build

---
+++ app/(app)/photography/black-n-white/page.tsx
@@ -0,0 +1,3 @@
+export default function BlackAndWhitePage() {
+	return <div>B&W Page </div>;
+}
--- app/(app)/photography/subject/fashion/page.tsx
+++ app/(app)/photography/subject/fashion/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-	return <div>Prints Page </div>;
+	return <div>Fashion Page </div>;
 }
